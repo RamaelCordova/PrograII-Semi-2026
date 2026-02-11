2205699eb29b740a4741e468b03b764cad4c0d04
Ramael Cordova <ramael.cordova14@gmail.com>
2026-02-11 17:37:00 -0600

    Le agregamos la opcion es de suma, resta, multiplicacion, division.

--- miPrimerProyecto/app/src/main/java/com/example/miprimerproyecto/MainActivity.java
+++ miPrimerProyecto/app/src/main/java/com/example/miprimerproyecto/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.miprimerproyecto;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
      TextView tempVal;
      Button btn;
+     RadioButton opt;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +26,24 @@ public class MainActivity extends AppCompatActivity {
         tempVal = findViewById(R.id.txtNum2);
         Double num2 = Double.parseDouble(tempVal.getText().toString());
 
-        double respuesta = num1 + num2;
+        double respuesta = 0;
 
+        opt = findViewById(R.id.optSuma);
+        if(opt.isChecked()) {
+            respuesta = num1 + num2;
+        }
+        opt = findViewById(R.id.optResta);
+        if(opt.isChecked()){
+            respuesta = num1 - num2;
+        }
+        opt = findViewById(R.id.optMultiplicar);
+        if(opt.isChecked()){
+            respuesta = num1 * num2;
+        }
+        opt = findViewById(R.id.optDividir);
+        if(opt.isChecked()){
+            respuesta = num1 / num2;
+        }
         tempVal = findViewById(R.id.lblRespuesta);
         tempVal.setText("Respuesta: "+ respuesta);
     }
