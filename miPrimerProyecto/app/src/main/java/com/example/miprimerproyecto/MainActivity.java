cf78d128fd5abed005e4e12e83fced5893713d90
Ramael Cordova <ramael.cordova14@gmail.com>
2026-03-19 14:18:10 -0600

    Uso del sensor de proximidad

--- miPrimerProyecto/app/src/main/java/com/example/miprimerproyecto/MainActivity.java
+++ miPrimerProyecto/app/src/main/java/com/example/miprimerproyecto/MainActivity.java
@@ -80,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
             }
+
+
         };
     }
 }
