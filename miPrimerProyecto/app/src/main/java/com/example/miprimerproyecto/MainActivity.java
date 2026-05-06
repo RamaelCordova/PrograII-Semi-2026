bf15ab8f483f3ccae3ec1a256415e4d135887395
ramae <ramael.cordova14@gmail.com>
2026-05-06 13:50:38 -0600

    Creacion de la rama conversores

--- miPrimerProyecto/app/src/main/java/com/example/miprimerproyecto/MainActivity.java
+++ miPrimerProyecto/app/src/main/java/com/example/miprimerproyecto/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.miprimerproyecto;
 
-import android.graphics.Color;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -19,56 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
-
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        iniciar();
-        super.onResume();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorAcelerometro();
-    }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-    private void sensorAcelerometro(){
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(sensor==null){
-            tempVal.setText("No dispones del sensor de acelerometro");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                double x = sensorEvent.values[0];
-                double y = sensorEvent.values[1];
-                double z = sensorEvent.values[2];
-                tempVal.setText("Desplazamiento: X: "+ x +"\n; Y: "+y +"\n; Z: "+z);
 
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
     }
 }
